aad5aab1bc8ef603cb38ed4156e191169bf6d8fa
DS-Ubuntu <96lives@gmail.com>
2018-05-24 23:01:52 +0900

    removed unecessary files

--- src/image_processing/DS/main.py
+++
@@ -1,9 +0,0 @@
-import colorContour
-import cv2
-import numpy as np
-
-if __name__ == "__main__":
-    in_dir = './data/test.MOV'
-    out_dir = './data/out.avi'
-    colorContour.detect_hand(in_dir, out_dir)
-    
