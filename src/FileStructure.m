fdca17ea6ff275dd00e95c35835f1deabd9089a9
DS-Ubuntu <96lives@gmail.com>
2018-06-05 15:10:10 +0900

    refactored classifier

---
+++ src/FileStructure.m
@@ -0,0 +1,22 @@
+CHEATING ROCK SCISSOR PAPER 
+
+File Structure
+
+1. finger_counter.py
+    Contains class "FingerCounter", which is the core class of the project.
+    This class counts and predicts the fingerCounter
+
+2. skin_detection.py
+    Contains functions for detecting skin
+
+3. background_subtractor.py
+    Contains class "BackgroundSubtractor", which subtracts the background to detect hand.
+    Since our dataset consists videos that dont have pure background, this method is only used for testing
+
+4. shaker.py 
+    Contains class "Shaker", which detects the 2 updown movement of the RPS game
+
+5. classifier.py
+    
+        
+
--- src/skin_color_classifier.py
+++ src/skin_color_classifier.py
@@ -24,62 +24,67 @@ class SkinColorClassifier():
 
         and_mask = cv2.bitwise_and(min_mask, max_mask)
         
-        start_time = time.time()
         # mask neg image of min_image
-        neg_img = cv2.bitwise_and(min_img, min_img, mask=and_mask)
-        reshaped_and_mask = and_mask.reshape(-1, 1)
-        neg_img = neg_img.reshape(-1, 3)
-        neg_bool = np.nonzero(reshaped_and_mask)
-        neg = neg_img[neg_bool[0]]
+        neg = self.collect_neg_data(min_img, and_mask)
 
         # mask pos image of min_image
-        pos_mask = min_mask - and_mask
-        pos_mask[pos_mask < 0] = 0
-        pos_img = cv2.bitwise_and(min_img, min_img, mask=pos_mask)
-        pos_img = pos_img.reshape(-1, 3)
-        reshaped_pos_mask = pos_mask.reshape(-1, 1)
-        pos_bool = np.nonzero(reshaped_pos_mask)
-        pos = pos_img[pos_bool[0]]    
-        #print("add count in min image: " + str(pos.shape))
-
-        print("Image processing time: " + str(time.time() - start_time))
+        pos = self.collect_pos_data(min_img, min_mask, and_mask)
         # mask pos image of max_image
-        pos_mask2 = max_mask - and_mask
-        pos_mask2[pos_mask2 < 0] = 0
-        pos_img2 = cv2.bitwise_and(max_img, max_img, mask=pos_mask2)
-        pos_img2 = pos_img2.reshape(-1, 3)
-        reshaped_pos_mask2 = pos_mask2.reshape(-1, 1)
-        pos_bool2 = np.nonzero(reshaped_pos_mask2)
-        pos = np.append(pos, pos_img2[pos_bool2[0]], axis=0)
-        #print("add count in min and max image: " + str(pos.shape))
+        #pos = np.append(pos, \
+        #    self.collect_pos_data(max_img, max_mask, and_mask), axis=0)
 
         return pos, neg
-    
-    
+
+
+    def collect_neg_data(self, img, and_mask):
+        
+        img = cv2.bitwise_and(img, img, mask=and_mask)
+        and_mask = and_mask.reshape(-1, 1)
+        img = img.reshape(-1, 3)
+        neg_bool = np.nonzero(and_mask)
+        return img[neg_bool[0]]
+
+    def collect_pos_data(self, img, img_mask, and_mask):
+
+        pos_mask = img_mask - and_mask
+        pos_mask[pos_mask < 0] = 0
+        pos_img = cv2.bitwise_and(img, img, mask=pos_mask)
+        img = pos_img.reshape(-1, 3)
+        pos_mask = pos_mask.reshape(-1, 1)
+        pos_bool = np.nonzero(pos_mask)
+        return img[pos_bool[0]]    
+      
     def mask_with_classifier(self, img):
 
         #blur_k_size = (11, 11)        
         #img = cv2.blur(img, blur_k_size)
-        
         w, h, c = img.shape
-        mask = sd.mask_skin(img)
+        skin_mask = sd.mask_skin(img)
        
-        reshaped_mask = mask.reshape(-1, 1)
-        candidates = (np.nonzero(reshaped_mask))[0]
+        # determine candidates for classification using mask
+        skin_mask_reshaped = skin_mask.reshape(-1, 1)
+        candidates = (np.nonzero(skin_mask_reshaped))[0]
 
+        # reshaped image for collecting pixels
         reshaped_img = img.reshape(-1, 3)
+        out = np.zeros(reshaped_img.shape)
 
+        # classify
         logits =  self.classifier.classify(reshaped_img[candidates])
+
+        # collect skin pixels
         skin_idx = np.nonzero(np.array(logits) == 1)
-        reshaped_img[candidates[skin_idx]] = [0, 0, 255]
-        out = reshaped_img.reshape(w, h, c)
+        out[candidates[skin_idx]] = 255
+        out = out.reshape(w, h, c)
         return out
 
 if __name__ == '__main__':
 
-    dir1 = "../data/img_min.png"
-    dir2 = "../data/img_max.png"
-    test_dir = "../data/img_test.png"
+
+    # test data
+    dir1 = "../test-data/img_min.png"
+    dir2 = "../test-data/img_max.png"
+    test_dir = "../test-data/img_test.png"
 
     min_img = cv2.imread(dir1)
     min_img = cv2.resize(min_img, None, fx=0.1, fy=0.1, interpolation=cv2.INTER_AREA)
