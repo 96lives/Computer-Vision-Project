ae6ef310b03d30d3e011ef7f1a71439e47401608
DS-Ubuntu <96lives@gmail.com>
2018-06-06 20:53:12 +0900

    finding error cause

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from finger_counter_tester import FingerCounterTester
 if __name__ == "__main__":
     
     in_dir = "../test-data/"
-    video_name = "IMG_0085.MOV"
+    video_name = "IMG_0090.MOV"
     out_dir = "../test-data/out/"
     report_name = "report.txt"
     fc = FingerCounterTester(video_name, report_name, in_dir, out_dir, True)
