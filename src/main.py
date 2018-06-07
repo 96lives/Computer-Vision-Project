f189a9dcd104297f1f541e0874e056d3c41db1e5
ds-home-ubuntu <96lives@gmail.com>
2018-06-07 23:29:08 +0900

    main modified

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from finger_counter2 import FingerCounter2
-from finger_counter3 import FingerCounter3
+#from new_finger_counter import NewFingerCounter as NFC
+from finger_counter import FingerCounter as FC
 
 if __name__ == "__main__":
     
-    in_dir = "../test-data/"
-    video_name = "IMG_0112.MOV"
-    out_dir = "../test-data/out/"
+    in_dir = "../data/S/"
+    video_name = "IMG_0036.MOV"
+    out_dir = "../data/out/"
     report_name = "report.txt"
-    fc = FingerCounter3(video_name, report_name, in_dir, out_dir, True)
+    fc = FC(video_name, report_name, in_dir, out_dir, True)
     fc.play_game()   
