d42b6ceee8f72324d2ebc12be79e2a5276141aec
Unknown <96lives@gmail.com>
2018-06-07 20:43:46 +0900

    new_finger_counter working

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from new_finger_counter import NewFingerCounter as FC
 if __name__ == "__main__":
     
     in_dir = "../data/hardP/"
-    video_name = "IMG_0050.MOV"
+    video_name = "IMG_0159.MOV"
     out_dir = "../data/out/"
     report_name = "report.txt"
     fc = FC(video_name, report_name, in_dir, out_dir, True)
